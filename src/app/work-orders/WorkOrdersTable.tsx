783ae57d6178e0e90941aac8a4096e626e52cc7a
dev-marceloborba <borba.marcelo@icloud.com>
2023-09-08 19:45:56 -0300

    feat: dynamic badge descriptor

--- src/app/work-orders/WorkOrdersTable.tsx
+++ src/app/work-orders/WorkOrdersTable.tsx
@@ -7,8 +7,9 @@ import Button from "../_components/Button";
 import WorkOrderFormModal from "./_components/WorkOrderFormModal";
 
 import BadgeStatus from "./_components/BadgeStatus";
+import { WorkOrder } from "./_models/work-order";
 
-export default function WorkOrdersTable({ data }: { data: any }) {
+export default function WorkOrdersTable({ data }: { data: WorkOrder[] }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [selectedItem, setSelectedItem] = useState<any>({
@@ -39,32 +40,36 @@ export default function WorkOrdersTable({ data }: { data: any }) {
             <Table.HeaderCell>Id</Table.HeaderCell>
             <Table.HeaderCell>Equipamento</Table.HeaderCell>
             <Table.HeaderCell>Data de criação</Table.HeaderCell>
+            <Table.HeaderCell>Data alvo</Table.HeaderCell>
             <Table.HeaderCell>Status</Table.HeaderCell>
             <Table.HeaderCell>Ações</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {data.map(({ id, equipmentName, date }: any) => (
-            <Table.Row key={id}>
-              <Table.Data>{id}</Table.Data>
-              <Table.Data>{equipmentName}</Table.Data>
-              <Table.Data>{date}</Table.Data>
-              <Table.Data>
-                <BadgeStatus />
-              </Table.Data>
-              <Table.Data>
-                <Button
-                  color="primary"
-                  onClick={() => handleSelectItem({ id, equipmentName, date })}
-                >
-                  Editar
-                </Button>
-                <Button color="danger" onClick={() => handleDeleteItem()}>
-                  Excluir
-                </Button>
-              </Table.Data>
-            </Table.Row>
-          ))}
+          {data.map(
+            ({ id, equipmentName, createdAt, target, workOrderStatus }) => (
+              <Table.Row key={id}>
+                <Table.Data>{id}</Table.Data>
+                <Table.Data>{equipmentName}</Table.Data>
+                <Table.Data>{createdAt?.toISOString()}</Table.Data>
+                <Table.Data>{target?.toISOString()}</Table.Data>
+                <Table.Data>
+                  <BadgeStatus status={workOrderStatus} />
+                </Table.Data>
+                <Table.Data>
+                  <Button
+                    color="primary"
+                    // onClick={() => handleSelectItem({ id, equipmentName, date })}
+                  >
+                    Editar
+                  </Button>
+                  <Button color="danger" onClick={() => handleDeleteItem()}>
+                    Excluir
+                  </Button>
+                </Table.Data>
+              </Table.Row>
+            )
+          )}
         </Table.Body>
       </Table.Root>
       <Modal
--- src/app/work-orders/_components/BadgeStatus.tsx
+++ src/app/work-orders/_components/BadgeStatus.tsx
@@ -1,7 +1,20 @@
-export default function BadgeStatus() {
+import { EWorkOrderStatus } from "../_models/work-order";
+
+export default function BadgeStatus({ status }: { status: EWorkOrderStatus }) {
+  const getEnumDescriptor = () => {
+    switch (status) {
+      case EWorkOrderStatus.IN_EXECUTION:
+        return "Em execução";
+      case EWorkOrderStatus.LATE:
+        return "Em atraso";
+      case EWorkOrderStatus.FINISHED:
+        return "Finalizada";
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center w-24 h-4 bg-blue-400 rounded text-white  font-semibold py-4 px-2">
-      Aprovada
+    <div className="flex justify-center items-center w-32 h-4 bg-blue-400 rounded text-white  font-semibold py-4 px-2">
+      {getEnumDescriptor()}
     </div>
   );
 }
